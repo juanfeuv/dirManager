ec6d84fb6558c51f239e9399412c4d22a5bab59a
Ubuntu <ubuntu@ip-172-31-43-153.us-east-2.compute.internal>
2021-05-01 02:07:55 +0000

    se agrega configuracion de deploy

---
+++ .deploy/mup.js
@@ -0,0 +1,64 @@
+module.exports = {
+  servers: {
+    one: {
+      // TODO: set host address, username, and authentication method
+      host: 'ec2-18-116-1-202.us-east-2.compute.amazonaws.com',
+      username: 'ubuntu',
+      pem: '../customKeys.pem'
+      // password: 'server-password'
+      // or neither for authenticate from ssh-agent
+    }
+  },
+
+  app: {
+    // TODO: change app name and path
+    name: 'dirManager',
+    path: '../',
+
+    servers: {
+      one: {},
+    },
+
+    buildOptions: {
+      serverOnly: true,
+    },
+
+    env: {
+      // TODO: Change to your app's url
+      // If you are using ssl, it needs to start with https://
+      PORT: 80,
+      ROOT_URL: 'http://ec2-18-116-1-202.us-east-2.compute.amazonaws.com'
+    },
+
+    docker: {
+      // abernix/meteord:node-12-base works with Meteor 1.9 - 1.10
+      // If you are using a different version of Meteor,
+      // refer to the docs for the correct image to use.
+      image: 'abernix/meteord:node-12-base',
+    },
+
+    // Show progress bar while uploading bundle to server
+    // You might need to disable it on CI servers
+    enableUploadProgressBar: true
+  },
+
+  mongo: {
+    version: '3.4.1',
+    servers: {
+      one: {}
+    }
+  },
+
+  // (Optional)
+  // Use the proxy to setup ssl or to route requests to the correct
+  // app when there are several apps
+
+  // proxy: {
+  //   domains: 'mywebsite.com,www.mywebsite.com',
+
+  //   ssl: {
+  //     // Enable Let's Encrypt
+  //     letsEncryptEmail: 'email@domain.com'
+  //   }
+  // }
+};
